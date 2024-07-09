340fac6f46ddcfd710ebcf4f2de6417c089b7390
WhaleOpop <al-agabani-kit@yandex.ru>
2024-07-09 09:54:51 +0300

    Update SaveLoad.gd
    Correction of errors related to the properties of stored objects "albedoColorMesh"

--- Scene/ConstructMap/3d/mapParts/SaveLoad.gd
+++ Scene/ConstructMap/3d/mapParts/SaveLoad.gd
@@ -1,103 +1,70 @@
 extends Node3D
 
-func saveObject():
-	var name_cell = get("metadata/NameCell")
+func saveObject() -> Dictionary:
+	var node_name = get("metadata/NameCell")
 	var dict = {
-		"nodeName": name_cell,
-		"position": position,
-		"rotation_degrees": rotation_degrees,
+		"nodeName": node_name,
+		"position": str(position),
+		"rotation_degrees": str(rotation_degrees)
 	}
-	
-	var albedo_colors = get_albedo_colors(name_cell)
-	for key in albedo_colors.keys():
-		dict[key] = albedo_colors[key]
+
+	match node_name:
+		"Forward", "Rotate", "Finish", "Checkpoint":
+			dict["albedoColorLine"] = str(get_child(1).get("surface_material_override/0").get("albedo_color"))
+			dict["albedoColorMesh"] = str(get_child(0).get("surface_material_override/0").get("albedo_color"))
+		"Color":
+			dict["albedoColor"] = str(get_child(0).get("surface_material_override/0").get("albedo_color"))
+		"Square":
+			dict["albedoColorMesh"] = str(get_child(0).get("surface_material_override/0").get("albedo_color"))
 
 	return dict
 
-func get_albedo_colors(name_cell):
-	var colors = {}
-	match name_cell:
-		"Forward", "Rotate", "Finish", "Checkpoint":
-			colors["albedoColorLine"] = get_child(1).get("surface_material_override/0").get("albedo_color")
-		"Forward", "Rotate", "Finish", "Square":
-			colors["albedoColorMesh"] = get_child(0).get("surface_material_override/0").get("albedo_color")
-		"Color", "Checkpoint":
-			colors["albedoColor"] = get_child(0).get("surface_material_override/0").get("albedo_color")
-		"Checkpoint":
-			colors["albedoColorMesh"] = get_child(0).get("surface_material_override/0").get("albedo_color")
-			colors["albedoColor"] = get_child(2).get("surface_material_override/0").get("albedo_color")
-	return colors
-
 func loadObject(loadedDict: Dictionary) -> void:
-	var matWhite = StandardMaterial3D.new()
-	var matBlack = StandardMaterial3D.new()
-	var matColor = StandardMaterial3D.new()
-	
-	matWhite.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	matBlack.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	matColor.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-
-	var name_cell = get("metadata/NameCell")
-
+	var node_name = loadedDict["nodeName"]
 	position = parseVec3(loadedDict["position"])
 	rotation_degrees = parseVec3(loadedDict["rotation_degrees"])
-	
-	set_albedo_colors(name_cell, loadedDict, matWhite, matBlack, matColor)
 
-func set_albedo_colors(name_cell, loadedDict, matWhite, matBlack, matColor):
-	match name_cell:
-		"Forward", "Finish":
-			matBlack.albedo_color = parseVec4(loadedDict["albedoColorLine"])
-			matWhite.albedo_color = parseVec4(loadedDict["albedoColorMesh"])
-			get_child(1).set("surface_material_override/0", matBlack)
-			get_child(0).set("surface_material_override/0", matWhite)
-		"Rotate":
-			matBlack.albedo_color = parseVec4(loadedDict["albedoColorLine"])
-			matWhite.albedo_color = parseVec4(loadedDict["albedoColorMesh"])
-			get_child(0).set("surface_material_override/0", matWhite)
-			get_child(1).set("surface_material_override/0", matBlack)
+	var mat_black = StandardMaterial3D.new()
+	var mat_white = StandardMaterial3D.new()
+	var mat_color = StandardMaterial3D.new()
+
+	mat_black.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	mat_white.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	mat_color.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+
+	match node_name:
+		"Forward", "Rotate", "Finish", "Checkpoint":
+			mat_black.albedo_color = parseVec4(loadedDict["albedoColorLine"])
+			mat_white.albedo_color = parseVec4(loadedDict["albedoColorMesh"])
+			get_child(1).set("surface_material_override/0", mat_black)
+			get_child(0).set("surface_material_override/0", mat_white)
 		"Color":
-			matColor.albedo_color = parseVec4(loadedDict["albedoColor"])
-			get_child(0).set("surface_material_override/0", matColor)
-		"Checkpoint":
-			matBlack.albedo_color = parseVec4(loadedDict["albedoColorLine"])
-			matWhite.albedo_color = parseVec4(loadedDict["albedoColorMesh"])
-			matColor.albedo_color = parseVec4(loadedDict["albedoColor"])
-			get_child(1).set("surface_material_override/0", matBlack)
-			get_child(0).set("surface_material_override/0", matWhite)
-			get_child(2).set("surface_material_override/0", matColor)
+			mat_color.albedo_color = parseVec4(loadedDict["albedoColor"])
+			get_child(0).set("surface_material_override/0", mat_color)
 		"Square":
-			matWhite.albedo_color = parseVec4(loadedDict["albedoColorMesh"])
-			get_child(0).set("surface_material_override/0", matWhite)
+			mat_white.albedo_color = parseVec4(loadedDict["albedoColorMesh"])
+			get_child(0).set("surface_material_override/0", mat_white)
 
-func parseVec4(strVec):
-	var vec4 = Color()
-	strVec = strVec.replace("(", "").replace(")", "")
-	var pos = strVec.split(",")
-	vec4.r = float(pos[0])
-	vec4.g = float(pos[1])
-	vec4.b = float(pos[2])
-	vec4.a = float(pos[3])
-	return vec4
-	
-func parseVec3(strVec):
-	var vec3 = Vector3()
-	strVec = strVec.replace("(", "").replace(")", "")
-	var pos = strVec.split(",")
-	vec3.x = float(pos[0])
-	vec3.y = float(pos[1])
-	vec3.z = float(pos[2])
-	return vec3
+	if node_name == "Checkpoint":
+		mat_color.albedo_color = parseVec4(loadedDict["albedoColor"])
+		get_child(2).set("surface_material_override/0", mat_color)
+
+func parseVec4(strVec: String) -> Color:
+	var vec4 = strVec.strip_edges().replace("(", "").replace(")", "").split(",")
+	return Color(vec4[0].to_float(), vec4[1].to_float(), vec4[2].to_float(), vec4[3].to_float())
+
+func parseVec3(strVec: String) -> Vector3:
+	var vec3 = strVec.strip_edges().replace("(", "").replace(")", "").split(",")
+	return Vector3(vec3[0].to_float(), vec3[1].to_float(), vec3[2].to_float())
 
 @onready var Car = preload("res://Scene/Car/Car.tscn")
 
 var isUsed = true
 
 func _on_area_3d_body_entered(body):
-	if body.get_class() == "VehicleBody3D":
-		if isUsed:
-			Globals.Checkpoint += 1
-			isUsed = false
+	if body.get_class() == "VehicleBody3D" and isUsed:
+		Globals.Checkpoint += 1
+		isUsed = false
 
 func reset():
 	isUsed = true
