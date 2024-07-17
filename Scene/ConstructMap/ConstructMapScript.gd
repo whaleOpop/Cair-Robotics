ab26c29257c54cf904cc51f9233371dae935e7fc
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-07-17 03:26:30 +0300

    Update ConstructMapScript.gd
    Optimized func and
    delete unused variables or functions

--- Scene/ConstructMap/ConstructMapScript.gd
+++ Scene/ConstructMap/ConstructMapScript.gd
@@ -1,11 +1,4 @@
 extends CanvasLayer
-"""
-Краткое описание:
-
-Это скрипт для создания карт в игре. Он использует библиотеки Godot Engine для работы с 3D-объектами
- и JSON для сохранения и загрузки данных. В программе используются различные элементы, такие как 
-направленная линия, поворот, цвета, финиш, контрольные точки и квадраты. Пользователь может выбирать
- элементы из списка и добавлять их на карту, а затем сохранять и загружать свои карты."""
 
 var MainScene = load("res://Scene/Menu/Main.tscn")
 
@@ -17,46 +10,33 @@ var MainScene = load("res://Scene/Menu/Main.tscn")
 @export var Square = preload( "res://Scene/ConstructMap/3d/mapParts/Square.tscn")
 @export var Box = preload( "res://Scene/ConstructMap/3d/mapParts/Box.tscn")
 @export var Buttons = preload("res://Scene/PopupMenu/popupMenu/btnEmpty/buttonEmpty.tscn")
+@export var AreaRemove = preload("res://Scene/ConstructMap/3d/mapParts/Area3dRemove/area_3d_remove.tscn")
 
+@onready var ConstructorMap = $Root3D/SubViewportContainer/SubViewport/ConstMap
 @onready var ConstMap = $Root3D/SubViewportContainer/SubViewport/ConstMap
+@onready var btnWB = $Root3D/Tiles_panel/TextureRect/TextureWB
+@onready var btnRGB =$Root3D/Tiles_panel/TextureRect/TextureRGB
 
-@onready var btnWB = $Root3D/PanelBlur/TextureRect/TextureWB
-@onready var btnRGB =$Root3D/PanelBlur/TextureRect/TextureRGB
+@onready var blur_panel =$Root3D/Tiles_panel/blur_panel
+
+@onready var load_panel =$Root3D/Tiles_panel/load_panel
+@onready var load_map_list = $Root3D/Tiles_panel/load_panel/TextureRect/ScrollContainer/map_list
+@onready var load_error_label = $Root3D/Tiles_panel/load_panel/TextureRect/load_error_label
+
+@onready var save_panel =$Root3D/Tiles_panel/save_panel
+@onready var save_error_label = $Root3D/Tiles_panel/save_panel/TextureRect/save_error_label
+@onready var save_text_edit = $Root3D/Tiles_panel/save_panel/TextureRect/save_text_edit
 
 var SelectItem = 0
-
-
+var btn_group = ButtonGroup.new()
+var loadname=""
 
 signal dataListCell(list)
 
 func _on_item_list_item_selected(index):
 	SelectItem = index
-		
-	if SelectItem == 0:
-		btnWB.visible=true
-		btnRGB.visible=false
-	elif SelectItem ==1:
-		btnWB.visible=true
-		btnRGB.visible=false
-	elif SelectItem ==2:
-		btnRGB.visible=true
-		btnWB.visible=false
-	elif SelectItem == 3:
-		btnWB.visible=true
-		btnRGB.visible=false
-	elif SelectItem == 4:
-		btnWB.visible=true
-		btnRGB.visible=true
-	elif SelectItem == 5:
-		btnWB.visible=true
-		btnRGB.visible=false
-	elif SelectItem == 6:
-		btnWB.visible=true
-		btnRGB.visible=false
-	
-			
-	print(SelectItem)
-	pass # Replace with function body.
+	btnWB.visible = SelectItem in [0, 1, 3, 4, 5, 6]
+	btnRGB.visible = SelectItem == 2 or SelectItem == 4
 
 
 
@@ -66,8 +46,7 @@ var wb = null
 var matWhite = StandardMaterial3D.new()
 var matBlack = StandardMaterial3D.new()
 
-@export var AreaRemove = preload("res://Scene/ConstructMap/3d/mapParts/Area3dRemove/area_3d_remove.tscn")
-@onready var ConstructorMap = $Root3D/SubViewportContainer/SubViewport/ConstMap
+
 	
 func _ready():
 	matBlack.shading_mode=BaseMaterial3D.SHADING_MODE_UNSHADED
@@ -78,146 +57,114 @@ func _ready():
 	matWhite.albedo_color=Color.WHITE
 	
 	pass
-	
-func _on_add_button_down():
-	
-	
-	if SelectItem == 0:
-	
-		var forward = Forward.instantiate()
-		var matLine = forward.get_child(0)
-		var matSquare = forward.get_child(1)
-		
-		if !wb:
-			matLine.set("surface_material_override/0",matWhite)
-			matSquare.set("surface_material_override/0",matBlack)
-		else:
-			matLine.set("surface_material_override/0",matBlack)
-			matSquare.set("surface_material_override/0",matWhite)
-		
-		get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(forward)
-		forward.add_to_group("save");
-	elif SelectItem==1:
-		var rotate = Rotate.instantiate()
-		var matLine = rotate.get_child(1)
-		var matSquare = rotate.get_child(0)
-		if !wb:
-			matLine.set("surface_material_override/0",matBlack)
-			matSquare.set("surface_material_override/0",matWhite)
-		else:
-			matLine.set("surface_material_override/0",matWhite)
-			matSquare.set("surface_material_override/0",matBlack)
-		get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(rotate)
-		rotate.add_to_group("save");
-	elif SelectItem==2:
-		var colors = Colors.instantiate()
-		var matColor = StandardMaterial3D.new()
-		var matMesh = colors.get_child(0)
-		matMesh.set("surface_material_override/0",matColor)
-		matColor.albedo_color=colorPicker
-		matColor.shading_mode=BaseMaterial3D.SHADING_MODE_UNSHADED
-		
-		get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(colors)
-		colors.add_to_group("save");
-	elif SelectItem==3:
-		var finish = Finish.instantiate()
-		var matSquare = finish.get_child(0)
-		var matLine = finish.get_child(1)
-		if !wb:
-			matLine.set("surface_material_override/0",matBlack)
-			matSquare.set("surface_material_override/0",matWhite)
-		else:
-			matLine.set("surface_material_override/0",matWhite)
-			matSquare.set("surface_material_override/0",matBlack)
-			
-		get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(finish)
-		finish.add_to_group("save");
-	elif SelectItem==4:
-		var checkPoint =CheckPoint.instantiate()
-		var matLine = checkPoint.get_child(1)
-		var matSquare = checkPoint.get_child(0)
-		var matCheckpoint = checkPoint.get_child(2)
-		var matColor = StandardMaterial3D.new()
-		if !wb:
-			matLine.set("surface_material_override/0",matBlack)
-			matSquare.set("surface_material_override/0",matWhite)
-		else:
-			matLine.set("surface_material_override/0",matWhite)
-			matSquare.set("surface_material_override/0",matBlack)
-		
-		matColor.albedo_color=colorPicker
-		matColor.shading_mode=BaseMaterial3D.SHADING_MODE_UNSHADED
-		matCheckpoint.set("surface_material_override/0",matColor)
-		get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(checkPoint)
-		checkPoint.add_to_group("save");
-	elif SelectItem==5:
-		var square= Square.instantiate()
-		var matSquare = square.get_child(0)
-		if !wb:
-			matSquare.set("surface_material_override/0",matWhite)
-		else:
-			matSquare.set("surface_material_override/0",matBlack)
-		get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(square)
-		square.add_to_group("save");
-	elif SelectItem==6:
-		var box= Box.instantiate()
-		var matSquare = box.get_child(0)
-		if !wb:
-			matSquare.set("surface_material_override/0",matWhite)
-		else:
-			matSquare.set("surface_material_override/0",matBlack)
-		get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(box)
-		box.add_to_group("save");
-	else:
-		SelectItem==null
-	
-	
-	pass # Replace with function body.
 
+func _on_add_button_down():
+	var instance
+	match SelectItem:
+		0:
+			instance = Forward.instantiate()
+			set_materials(instance, 0, 1)
+		1:
+			instance = Rotate.instantiate()
+			set_materials(instance, 0, 1)
+		2:
+			instance = Colors.instantiate()
+			set_color_material(instance)
+		3:
+			instance = Finish.instantiate()
+			set_materials(instance, 0, 1)
+		4:
+			instance = CheckPoint.instantiate()
+			set_materials(instance, 1, 0, 2)
+		5:
+			instance = Square.instantiate()
+			set_single_material(instance, 0)
+		6:
+			instance = Box.instantiate()
+			set_single_material(instance, 0)
+		_:
+			SelectItem = null
+			return
+
+	get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(instance)
+	instance.add_to_group("save")
+
+func set_materials(instance, mat_index1, mat_index2, extra_mat_index=-1):
+	var mat1 = instance.get_child(mat_index1)
+	var mat2 = instance.get_child(mat_index2)
+	if !wb:
+		mat1.set("surface_material_override/0", matWhite)
+		mat2.set("surface_material_override/0", matBlack)
+	else:
+		mat1.set("surface_material_override/0", matBlack)
+		mat2.set("surface_material_override/0", matWhite)
+	if extra_mat_index != -1:
+		set_color_material(instance, extra_mat_index)
+
+func set_single_material(instance, mat_index):
+	var mat = instance.get_child(mat_index)
+	if !wb:
+		mat.set("surface_material_override/0", matWhite)
+	else:
+		mat.set("surface_material_override/0", matBlack)
+
+func set_color_material(instance, mat_index=0):
+	var matMesh = instance.get_child(mat_index)
+	var matColor = StandardMaterial3D.new()
+	matMesh.set("surface_material_override/0", matColor)
+	matColor.albedo_color = colorPicker
+	matColor.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
 
 func _on_wb_toggled(button_pressed):
 	wb=button_pressed
 	pass # Replace with function body.
 
-
 func _on_rgb_color_changed(color):
 	colorPicker=color
 	pass # Replace with function body.
 
-
 func _on_save_button_down():
-	$Root3D/PanelBlur/PanelBlur2.show()
-	$Root3D/PanelBlur/PanelBlur4.show()
+	blur_panel.show()
+	save_panel.show()
 	pass # Replace with function body.
 
 func _on_ok_pressed():
 	
-	var textedit = $Root3D/PanelBlur/PanelBlur4/TextureRect/TextEdit.text
+	var textedit = save_text_edit.text
 	
 	if textedit!="":
-		_save(textedit)
-		$Root3D/PanelBlur/PanelBlur2.hide()
-		$Root3D/PanelBlur/PanelBlur4.hide()
-		$Root3D/PanelBlur/PanelBlur4/TextureRect/Label.text=""
+		if _checkFinishTile():
+			_save(textedit)
+			blur_panel.hide()
+			save_panel.hide()
+
+			save_error_label.text=""
+		else:
+			save_error_label.text="Нет финиша"
+			textedit=""
 	else:
-		$Root3D/PanelBlur/PanelBlur4/TextureRect/Label.text="Введите название карты"
+		save_error_label.text="Введите название карты"
+		textedit=""
 	
 	pass # Replace with function body.
-
+func _clearMap():
+	for i in ConstMap.get_children():
+		if i.get("metadata/NameCell"):
+			i.queue_free()
+	ConstructorMap.add_child(AreaRemove.instantiate())
+	pass
+func _checkFinishTile():
+	for i in ConstMap.get_children():
+		if i.get("metadata/NameCell"):
+			if i.get("metadata/NameCell")=="Finish":
+				return true
+	return false
 
 func _on_cancel_pressed():
-	$Root3D/PanelBlur/PanelBlur2.hide()
-	$Root3D/PanelBlur/PanelBlur4.hide()
-	$Root3D/PanelBlur/PanelBlur4/TextureRect/Label.text=""
+	blur_panel.hide()
+	save_panel.hide()
+	save_error_label.text=""
 	pass # Replace with function body.
-
-
-func _on_panel_hidden():
-	
-	pass # Replace with function body.
-
-
-
 
 func _save(filename) -> void:
 	var save_file = FileAccess.open("user://"+filename+".cair", FileAccess.WRITE) # Open File
@@ -238,76 +185,72 @@ func _save(filename) -> void:
 	
 	save_file.close() # Close File
 	pass
-	
-func _load(filename) -> void:
+func _load(filename: String) -> void:
 	# Check if the SaveFile exists
-	if !filename:
+	if filename == "":
+		print("Error, filename is empty.")
 		return
-	if !FileAccess.file_exists("user://"+filename):
+	
+	var file_path = "user://" + filename
+	if !FileAccess.file_exists(file_path):
 		print("Error, no Save File to load.")
 		return
-		
-	var save_file = FileAccess.open("user://"+filename, FileAccess.READ) # Open File
+	
+	var save_file = FileAccess.open(file_path, FileAccess.READ) # Open File
+	if save_file == null:
+		print("Error, could not open Save File.")
+		return
+	
+	var root_node = get_node("Root3D/SubViewportContainer/SubViewport/ConstMap")
 	
 	while save_file.get_position() < save_file.get_length():
 		# Get the saved dictionary from the next line in the save file
 		var json = JSON.new()
-		json.parse(save_file.get_line())
+		var line = save_file.get_line()
+		var err = json.parse(line)
+		if err != OK:
+			print("Error parsing JSON: ", line)
+			continue
 		
 		# Get the Data
 		var node_data = json.get_data()
+		if not node_data.has("nodeName"):
+			print("Error: 'nodeName' not found in data.")
+			continue
 		
-		print(node_data["nodeName"])
-		if node_data["nodeName"]=="Forward":
-			var forward = Forward.instantiate()
-			forward.loadObject(node_data)
-			get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(forward)
-			forward.add_to_group("save");
-			#get_node(node_data["nodeName"]).loadObject(node_data)
-		elif node_data["nodeName"]=="Rotate":
-			var rotate = Rotate.instantiate()
-			rotate.loadObject(node_data)
-			get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(rotate)
-			rotate.add_to_group("save");
-			#get_node(node_data["nodeName"]).loadObject(node_data)
-		elif node_data["nodeName"]=="Color":
-			var colors = Colors.instantiate()
-			colors.loadObject(node_data)
-			get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(colors)
-			colors.add_to_group("save");
-			#get_node(node_data["nodeName"]).loadObject(node_data)
-		elif node_data["nodeName"]=="Checkpoint":
-			var checkPoint = CheckPoint.instantiate()
-			checkPoint.loadObject(node_data)
-			get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(checkPoint)
-			checkPoint.add_to_group("save");
-			#get_node(node_data["nodeName"]).loadObject(node_data)
-			
-		elif node_data["nodeName"]=="Finish":
-			var finish = Finish.instantiate()
-			finish.loadObject(node_data)
-			get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(finish)
-			#get_node(node_data["nodeName"]).loadObject(node_data)
-			finish.add_to_group("save");
-		elif node_data["nodeName"]=="Square":
-			var square = Square.instantiate()
-			square.loadObject(node_data)
-			get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(square)
-			square.add_to_group("save");
-		elif node_data["nodeName"]=="Box":
-			var box = Box.instantiate()
-			box.loadObject(node_data)
-			get_node("Root3D/SubViewportContainer/SubViewport/ConstMap").add_child(box)
-			box.add_to_group("save");
+		var node_instance = null
 		
+		match node_data["nodeName"]:
+			"Forward":
+				node_instance = Forward.instantiate()
+			"Rotate":
+				node_instance = Rotate.instantiate()
+			"Color":
+				node_instance = Colors.instantiate()
+			"Checkpoint":
+				node_instance = CheckPoint.instantiate()
+			"Finish":
+				node_instance = Finish.instantiate()
+			"Square":
+				node_instance = Square.instantiate()
+			"Box":
+				node_instance = Box.instantiate()
+			_:
+				print("Unknown node type: ", node_data["nodeName"])
+				continue
 		
+		if node_instance:
+			node_instance.loadObject(node_data)
+			root_node.add_child(node_instance)
+			node_instance.add_to_group("save")
+	
 	save_file.close() # Close File
-	pass
 
-var btn_group = ButtonGroup.new()
+
 func _on_load_pressed():
-	$Root3D/PanelBlur/PanelBlur2.show()
-	$Root3D/PanelBlur/PanelBlur3.show()
+	
+	blur_panel.show()
+	load_panel.show()
 	var mapsNAme =getMapFile() 
 	var buttons 
 	for i in mapsNAme:
@@ -316,52 +259,45 @@ func _on_load_pressed():
 		
 		buttons.get_child(0).text=i
 		buttons.get_child(0).set("button_group",btn_group)
-		$Root3D/PanelBlur/PanelBlur3/TextureRect/ScrollContainer/VScrollBar.add_child(buttons)
+		load_map_list.add_child(buttons)
 	for i in btn_group.get_buttons():
 		i.connect("pressed",getName)
 		
 	pass # Replace with function body.
 
-var loadname=""
 func getName():
-	
 	loadname=btn_group.get_pressed_button().text
-	
 	pass
 
-
 func _on_back_pressed():
-	
 	get_tree().change_scene_to_packed(MainScene)
 	pass # Replace with function body.
 
-
 func _on_o_kload_pressed():
-
-	if loadname!="":
+	if loadname != "":
+	
+		_clearMap()
 		_load(loadname)
-		$Root3D/PanelBlur/PanelBlur2.hide()
-		$Root3D/PanelBlur/PanelBlur3.hide()
-		$Root3D/PanelBlur/PanelBlur3/TextureRect/Label.text=""
-		var children = $Root3D/PanelBlur/PanelBlur3/TextureRect/ScrollContainer/VScrollBar.get_children()
+		blur_panel.hide()
+		load_panel.hide()
+		load_error_label.text = ""
+		
+
+		var children = load_map_list.get_children()
 		for i in children:
 			i.queue_free()
 	else:
-		$Root3D/PanelBlur/PanelBlur3/TextureRect/Label.text="Выберите карту которую хотите редактировать"
-	pass # Replace with function body.
-
-
+		load_error_label.text = "Выберите карту которую хотите редактировать"
 
 func _on_cancelload_pressed():
-	$Root3D/PanelBlur/PanelBlur2.hide()
-	$Root3D/PanelBlur/PanelBlur3.hide()
-	$Root3D/PanelBlur/PanelBlur3/TextureRect/Label.text=""
-	var children = $Root3D/PanelBlur/PanelBlur3/TextureRect/ScrollContainer/VScrollBar.get_children()
+	blur_panel.hide()
+	load_panel.hide()
+	load_error_label.text=""
+	var children = load_map_list.get_children()
 	for i in children:
 		i.queue_free()
 	pass # Replace with function body.
-	
-		
+
 func getMapFile():
 	var files = []
 	var dir = DirAccess.open("user://")
@@ -379,3 +315,6 @@ func getMapFile():
 	dir.list_dir_end()
 
 	return files
+
+
+
