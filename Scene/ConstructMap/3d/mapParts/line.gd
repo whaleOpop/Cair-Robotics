f214e868c9ac56ce4c7af94a5671bd1c3a09016d
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-07-24 00:03:22 +0300

    Update line.gd
    change get function material

--- Scene/ConstructMap/3d/mapParts/line.gd
+++ Scene/ConstructMap/3d/mapParts/line.gd
@@ -1,6 +1,6 @@
-extends StaticBody3D
+extends CSGBox3D
 
 func _get_color():
-	var mat = $"..".get_surface_override_material(0)
+	var mat = get("material_override")
 	return mat.get("albedo_color")
 	
